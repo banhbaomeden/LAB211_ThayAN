7c19e8d04c52c25090e4238d5a2ac147aa366394
banhbaomeden <leh17693@gmail.com>
2026-03-12 16:05:08 +0700

    Add files via upload

---
+++ src/dto/PersonDTO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author admin
+ */
+public class PersonDTO {
+    private String name;
+    private String address;
+    private double salary;
+
+    public PersonDTO() {
+    }
+    
+    public PersonDTO(String name, String address, double salary) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+}
